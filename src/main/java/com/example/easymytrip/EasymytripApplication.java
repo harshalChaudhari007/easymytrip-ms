435c13f26caf9a2f3bc0cd8c40acfb4f7c8be2c0
hchaudhari007 <hchaudhari007@gmail.com>
2023-01-05 22:13:06 +0530

    CodeRefactor

--- src/main/java/com/example/easymytrip/EasymytripApplication.java
+++ src/main/java/com/example/easymytrip/EasymytripApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EasymytripApplication {
+public class
+
+EasymytripApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasymytripApplication.class, args);
